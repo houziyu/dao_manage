1a1ed7fce220c17731989bde748780c922e181cb
yunque <469910799@qq.com>
2017-08-16 11:29:35 +0800

    log finish

--- main/tests.py
+++ main/tests.py
@@ -3,14 +3,12 @@ import datetime
 import docker
 
 
-docker_singleton = docker.DockerClient(base_url='tcp://192.168.1.147:2375')
-
-aa= datetime.datetime.now() + datetime.timedelta(days=-1)
-
-docker_list  = docker_singleton.containers.list()[0].logs(timestamps)
-print(docker_list)
-
-
-
-print(type(aa))
+docker_singleton = docker.DockerClient(base_url='tcp://47.92.136.150:2375')
+aa= datetime.datetime.now() + datetime.timedelta(hours=-1)
 print(aa)
+bb = []
+docker_list  = docker_singleton.containers.list()
+for i in docker_list:
+    if i.name == 'order-service-16':
+        bb.append(i)
+print(bb[0].logs(since=aa))
