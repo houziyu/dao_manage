702f05ee5c44d414bb6ab17cf8175d5ce0c1f1e5
yunque <469910799@qq.com>
2017-08-21 16:07:27 +0800

    log wancheng

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate,logout,login
 from django.utils.safestring import mark_safe
 from django.contrib.auth.decorators import login_required
 from main.lib import docker_initial
+from config import dao_config
+import datetime
 # Create your views here.
 
 def index(request):
@@ -52,3 +54,24 @@ def logs(request):
         info={'logs':logs_str,'hostname':hostname,'container_name':container_name}
         return render(request, 'logs.html', info)
         #获取到了容器的name 然后去lib中搜索name的容器然后进行日志打印
+
+@login_required(login_url='/login/')
+def cron_download_log():
+    docker_container_all = docker_initial().docker_container_dictionary()
+    for i in docker_container_all:
+        hostname= i
+        for y in docker_container_all[i]:
+            service_name = y.name.split('-')[0]
+            if y.status == 'running':
+                if service_name in dao_config.service_name_list:
+                    aaaaa_date = str(datetime.datetime.now())
+                    #时间测试
+                    log_date = str(datetime.date.today() + datetime.timedelta(days=-1, hours=+8))
+                    service_log_path = '/logs/' + service_name + '-service' + '/info/log-info-' + log_date + '.0.log'
+                    log_init = y.get_archive(service_log_path)
+                    log_str = str(log_init[0].data, encoding="utf-8")
+                    log_local_name = '/log_everyone_bak/' + service_name + '-service/' + hostname + '-' + service_name + '-service' + '-' + log_date + '.log'
+                    log_file = open(log_local_name, 'a+')
+                    log_file.write(aaaaa_date)
+                    log_file.write(log_str)
+                    log_file.close()
