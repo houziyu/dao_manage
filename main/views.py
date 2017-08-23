9fcf5d2a6756e0f8a3acc8fb392502c46708b92d
yunque <469910799@qq.com>
2017-08-23 10:13:44 +0800

    log wancheng

--- main/views.py
+++ main/views.py
@@ -72,4 +72,4 @@ def update_log(request):
                     log_file.write('执行时间:' + date_now)
                     log_file.write(log_str)
                     log_file.close()
-    return HttpResponse('完成')
+    return HttpResponse("<a href='{% url 'dashboard' %}'>docker仪表盘</a>")
