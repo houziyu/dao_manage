63ab26c4a25d2bde68952caf711cd5853b64243c
yunque <469910799@qq.com>
2017-08-24 15:37:56 +0800

    log wancheng

--- dao_manage/urls.py
+++ dao_manage/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from main import views
-
 urlpatterns = [
     url(r'^$', views.index,name='index'),
     url(r'^admin/', admin.site.urls),
